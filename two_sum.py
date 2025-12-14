a6acb84a1192df11cd4c0356a184dcd77f66fe3d
AISWARYAs6282 <Aiswaryaprakasan3333@gmail.com>
2025-12-14 21:17:13 +0530

    Initial commit - LeetCode setup

---
+++ two_sum.py
@@ -0,0 +1,17 @@
+# LeetCode 1: Two Sum
+# Time: O(n)
+# Space: O(n)
+
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2, 7, 11, 14], 9))
