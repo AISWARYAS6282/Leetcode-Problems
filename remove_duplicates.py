d69510b060a333cb1bb25acbcf6d15197f261442
AISWARYAs6282 <Aiswaryaprakasan3333@gmail.com>
2025-12-15 15:35:16 +0530

    Fix Remove Duplicates logic

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -11,3 +11,9 @@ class Solution:
                 k += 1
 
         return k
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    k = sol.removeDuplicates(nums)
+    print (f"k={k} \nSorted array updated={nums[:k]}")
+  # Output: 5
